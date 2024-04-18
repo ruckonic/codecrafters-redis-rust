c95fe162f9f8f0c925c3bc972376ed40ab73ab83
Rene Corrales <ruckonic@icloud.com>
2024-04-18 14:05:39 -0600

    concurrent clients

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,50 @@
-use std::io::{Read, Result, Write};
-use std::net::{TcpListener, TcpStream};
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt, Result},
+    net::{TcpListener, TcpStream},
+};
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+#[tokio::main]
+async fn main() -> Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:6379").await?;
 
-    for stream in listener.incoming() {
+    loop {
+        let stream = listener.accept().await;
+
         match stream {
-            Ok(mut _stream) => loop {
-                let mut buffer = [0; 1024];
-                let read_bytes = _stream.read(&mut buffer).expect("Error reading bites");
-
-                if read_bytes == 0 {
-                    break;
-                }
-
-                ping(&mut _stream).unwrap();
-            },
+            Ok((mut stream, _)) => {
+                println!("New connection");
+                let _ = tokio::spawn(async move {
+                    process_incomming_connections(&mut stream).await.unwrap();
+                });
+            }
             Err(e) => {
-                eprintln!("error: {}", e);
+                println!("err: {}", e);
             }
         }
     }
+}
+
+async fn process_incomming_connections(stream: &mut TcpStream) -> Result<()> {
+    loop {
+        let mut buffer = [0; 1024];
+        let f = stream.read(&mut buffer).await?;
+
+        if f == 0 {
+            break;
+        }
+
+        ping(stream).await?;
+        println!("{}", String::from_utf8_lossy(&buffer));
+    }
+
+    Ok(())
 }
 
 /// Write PONG response in TcpStream
 ///
 /// # Panics
 /// Panics if can't write in TcpStream
-fn ping(stream: &mut TcpStream) -> Result<()> {
-    stream.write(b"+PONG\r\n")?;
+async fn ping(stream: &mut TcpStream) -> Result<()> {
+    stream.write(b"+PONG\r\n").await?;
     Ok(())
 }
